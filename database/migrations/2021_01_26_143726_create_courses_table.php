2cf0f17ed0752ed637527d9b124ff9d42c10a9bf
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-01-26 08:48:18 -0600

    Modified courses migration structure

--- database/migrations/2021_01_26_143726_create_courses_table.php
+++ database/migrations/2021_01_26_143726_create_courses_table.php
@@ -15,7 +15,23 @@ class CreateCoursesTable extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');//Que usuario ha creado este curso
+            $table->unsignedBigInteger('category_id');//A que categoría pertenece éste curso
+
+            $table->string('name');
+            $table->string('slug');//Identificador único amigable para la lectura
+            $table->string('image');
+            $table->text('description');
             $table->timestamps();
+
+            //Relaciones (llaves foraneas)
+            /**
+             * Vamos a trbajar con la tabla users, vamos a relacionar su campo id
+             * con el campo foreign 'user_id'
+             */
+            $table->foreign('user_id')->references('id')->on('users');
+            
+            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
